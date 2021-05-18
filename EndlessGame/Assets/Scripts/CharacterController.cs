55a9ef0c960cfec21a4c093dc21cac68c3cf3955
Trax <trax.temu@gmail.com>
2021-05-18 23:41:36 +0300

    Kentän borderia yritin parenella
    jäi pahasti kesken ja Raipe kattoo ne läpi ku ehtii :)

--- EndlessGame/Assets/Scripts/CharacterController.cs
+++ EndlessGame/Assets/Scripts/CharacterController.cs
@@ -7,7 +7,9 @@ public class CharacterController : MonoBehaviour
     public float movementSpeed = 10f;
     public SpawnManager spawnManager;
     public int points = 0;
-    
+    //private float xMin = -13.2f, xMax = -1.0f;
+    //private float timeValue = 0.0f;
+    //Vector3 playerPos = new Vector3(0, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +25,50 @@ public class CharacterController : MonoBehaviour
 
         transform.Translate(new Vector3(hMovement, 0, vMovement) * Time.deltaTime);
 
-    }
+        //transform.position = new Vector3(Mathf.Clamp(transform.position.x, -13.2f, -1f), transform.position.y -53f, 9999f);
+
+        // Compute the sin position.
+        //float xValue = Mathf.Sin(timeValue * 5.0f);
+
+        // Now compute the Clamp value.
+        //float xPos = Mathf.Clamp(xValue, xMin, xMax);
+
+        // Update the position of the cube.
+       // transform.position = new Vector3(xPos, -13.2f, -1f);
+
+        // Increase animation time.
+       // timeValue = timeValue + Time.deltaTime;
+
+        // Reset the animation time if it is greater than the planned time.
+       // if (xValue > Mathf.PI * 2.0f)
+        
+           // timeValue = 0.0f;
+
+        }
+
+   // public void userInput(int direction)
+   // {
+       // Vector3 tempPos = playerPos;
+      //  switch (direction)
+    //    {
+         //   case 0:
+        //        tempPos.y++;
+       //         break;
+      //      case 1:
+       //         tempPos.y--;
+       //         break;
+       //     case 2:
+       //         tempPos.x++;
+       //         break;
+       //     case 3:
+        //        tempPos.x--;
+        //        break;
+      //  }
+        
+      //  tempPos.x = Mathf.Clamp(tempPos.y, -13.2f, -1f);
+
+     //   playerPos = tempPos;
+   // }
 
     private void OnTriggerEnter(Collider other)
     {
