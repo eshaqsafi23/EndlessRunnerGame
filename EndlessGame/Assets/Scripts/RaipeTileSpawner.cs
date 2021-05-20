032d116955ae964adecbaec7aca188b070f67185
RaipePerkele <robertheinonen1@gmail.com>
2021-05-20 19:28:14 +0300

    testailua
    perkele ku ei toimi >:L

---
+++ EndlessGame/Assets/Scripts/RaipeTileSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class RaipeTileSpawner : MonoBehaviour
+{
+    public GameObject RoadTileTEST;
+    Vector3 NextSpawnPoint;
+
+    public void SpawnTile()
+    {
+        GameObject temp = Instantiate(RoadTileTEST, NextSpawnPoint, Quaternion.identity);
+        NextSpawnPoint = temp.transform.GetChild(1).transform.position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 300; i++)
+        {
+            SpawnTile();
+        }
+    }
+}
---
+++ EndlessGame/Assets/Scripts/RoadTileTEST.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class RoadTileTEST : MonoBehaviour
+{
+    RaipeTileSpawner groundSpawner;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        groundSpawner = GameObject.FindObjectOfType<RaipeTileSpawner>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        groundSpawner.SpawnTile();
+        Destroy(gameObject, 2);
+    }
+}
