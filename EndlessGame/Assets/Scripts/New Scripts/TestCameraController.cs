18daa7f770a203b0895c3dc9d5395b634ebf3c09
Esa <57177035+esafi23@users.noreply.github.com>
2021-05-20 08:15:52 +0300

    Sky

---
+++ EndlessGame/Assets/Scripts/New Scripts/TestCameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCameraController : MonoBehaviour
+{
+    private Transform player;
+
+    private float yOffset = 4;
+    private float zOffset = -10f ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x,player.position.y + yOffset,player.position.z + zOffset);
+    }
+}
