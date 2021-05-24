b1dd302e411f297818baf465f2929a8ad7330418
Paavo Gaidach <paavo.gaidach@gmail.com>
2021-05-24 14:35:42 +0300

    Cleared charactercontroller bugs
    Added cross platform input from standard assets

--- EndlessGame/Assets/Scripts/CharacterController.cs
+++ EndlessGame/Assets/Scripts/CharacterController.cs
@@ -1,38 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.EventSystems;
 
 
-public class CharacterController : MonoBehaviour
+public class CharacterController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
+    public bool buttonPressed;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        buttonPressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        buttonPressed = false;
+    }
     public float movementSpeed = 10f;
     public SpawnManager spawnManager;
     public int points = 0;
-
-
     public float autoMoveSpeed;
     public ParticleSystem GunFire;
 
 
-
     void Start()
     {
-        
+
     }
 
-
     void Update()
-
-    {
-        float hMovement = Input.GetAxis("Horizontal") * movementSpeed / 2;
-        float vMovement = Input.GetAxis("Vertical") * movementSpeed;
-
     {   // Antinjutut
         // Sivuliike
-        float hMovement = Input.GetAxis("Horizontal") * movementSpeed / 2;
-       
-        //crossplatform
-        
+        float hMovement = CrossPlatformInputManager.GetAxis("Horizontal") * movementSpeed / 2;
+
+
         //Sivuliikkeen rajoitus
         if (transform.position.x < -14 && hMovement < 0)
         {
@@ -44,31 +47,55 @@ public class CharacterController : MonoBehaviour
             hMovement = 0;
         }
 
-
-        transform.Translate(new Vector3(hMovement, 0, vMovement) * Time.deltaTime);
-
+        // Liikuttaa pelaajaa, sivuliike GetAxis, eteenpäinliike säädetään itse autoMoveSpeed
+        transform.Translate(new Vector3(hMovement, 0, autoMoveSpeed) * Time.deltaTime);
 
 
         // Antin ampuminen
         if (Input.GetButtonDown("Fire1"))
         {
-                 //Soitetaan partikkeliefekti pyssystä
-                  GunFire.Play();
+            //Soitetaan partikkeliefekti pyssystä
+            GunFire.Play();
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject objectHit = hit.transform.gameObject; // esim. kaktus
+
+                // Lisäys katsotaan että rigidbody ei ole jo olemassa.
+                if (objectHit.CompareTag("Shootable") && (objectHit.GetComponent<Rigidbody>() == null))
+                {
+
+                    Rigidbody objRB = objectHit.AddComponent<Rigidbody>();
+                    objRB.mass = 0.1f;
+                    Vector3 shootDirection = objectHit.transform.position - gameObject.transform.position;
 
 
-        }
+                    objRB.AddForceAtPosition(shootDirection, hit.point);
 
-  
                 }
             }
         }
+
     }
-       
+
+    public void MoveLeft()
+    {
+        transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+    }
+    public void MoveRight()
+    {
+        transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+    }
+
+
 
     private void OnTriggerEnter(Collider other)
     {
         spawnManager.SpawnTriggerEntered();
-     
+
     }
     private void OnGUI()
     {
