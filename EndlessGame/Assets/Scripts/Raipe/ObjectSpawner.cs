9e98d20c84bb75f42afe46660fbe4d4c327aaca4
RaipePerkele <robertheinonen1@gmail.com>
2021-05-24 22:29:31 +0300

    testing >:L

---
+++ EndlessGame/Assets/Scripts/Raipe/ObjectSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    private bool spawningObject = false;
+
+    // Might need to play around with the value
+    [SerializeField] private float groundSpawnDistance = 50f;
+
+    public static ObjectSpawner instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void SpawnGround()
+    {
+        ObjectPooler.instance.SpawnFromPool("ground", new Vector3(0, 0, groundSpawnDistance), Quaternion.identity);
+    }
+}
